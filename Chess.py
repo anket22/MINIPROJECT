d9f636d5e39c684d609df174b138b9b76fde9003
anket22 <102409790+anket22@users.noreply.github.com>
2022-06-30 11:27:49 +0530

    Add files via upload

---
+++ Chess.py
@@ -0,0 +1,61 @@
+import pygame as p
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+board = [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+                      ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+                      ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
+def load_images():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+
+    load_images()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        # draw_game_state(screen, board)
+        draw_board(screen, board)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+'''
+Responsible for all the graphivs within a current game state
+'''
+def draw_game_state(screen, gs):
+    draw_board(screen)
+    # draw_pieces(screen, board)
+
+'''
+Draw the squares on the board
+'''
+def draw_board(screen, board):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
